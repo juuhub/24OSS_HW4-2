61fc5b320a5a6f7f966eabf156432761c5898c9a
juuhub <juhankim@handong.ac.kr>
2024-10-05 14:58:49 +0900

    minor change

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -3,20 +3,16 @@ import './Common.css';
 
 const Footer = () => {
   return (
-    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div className="col-md-4 d-flex align-items-center">
-      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-        <svg className="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
-      </a>
-      <span className="mb-3 mb-md-0 text-body-secondary">&copy; 2024 Company, Inc</span>
-    </div>
-
-    <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-      <li className="ms-3"><a className="text-body-secondary" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
-      <li className="ms-3"><a className="text-body-secondary" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
-      <li className="ms-3"><a className="text-body-secondary" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
-    </ul>
-  </footer>
+    <footer className="footer py-3 my-4">
+      <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
+        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
+        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
+        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
+        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
+      </ul>
+      <p className="text-center text-body-secondary">&copy; 2024 Company, Inc</p>
+    </footer>
   );
 };
 
